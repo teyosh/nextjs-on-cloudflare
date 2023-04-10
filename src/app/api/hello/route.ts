bf03badcf408c7d092973ffe606b67b0aec260e0
Tessei Yoshida <yosh256@gmail.com>
2023-04-10 14:07:19 +0900

    edge

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,3 +1,7 @@
+export const config = {
+  runtime: 'edge',
+};
+
 export async function GET(request: Request) {
-  return new Response('Hello, Next.js!')
+  return new Response('Hello, Next.js!');
 }
